8e24db8bace1353729f408305225ce7e97db57c1
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-04-16 13:32:06 -0700

    when given a sorted list will use divide and conquer O(log n) to find the floor or equivalent value of n

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,28 @@
-function findFloor() {
-  
-}
+function findFloor(arr, num) {
+    let left = 0;
+    let right = arr.length - 1;
+    let middle = null;
+
+    if(arr[right] <= num) return arr[right];
+    if(arr[left] > num) return -1;
+
+    while(left <= right) {
+        middle = Math.floor((left + right) / 2);
+
+        if(arr[middle] === num) return arr[middle];
+        
+        if(left === right && arr[middle] < num) return arr[middle];
+
+        if(left === right && arr[middle] > num) return arr[middle - 1];
+
+        if(arr[middle] > num) {
+            if(arr[middle - 1] < num) return arr[middle - 1];
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    };
+    return -1;
+};
 
 module.exports = findFloor
