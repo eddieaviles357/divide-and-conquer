5a3f57e19ff631fd9f4c3df9f6277f41b2ee7e85
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-04-17 16:02:06 -0700

    index will be returned when n is found in a rotated index

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,25 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex(arr, num) {
+    let left = 0;
+    let right = arr.length - 1;
+    let middle = null;
+
+    while (left <= right) {
+        middle = Math.floor((left + right) / 2);
+        
+        if(arr[middle] === num) {
+            return middle;
+        } 
+
+        if(arr[middle] > num && arr[right] < num) {
+            right = middle - 1;
+        } else if(arr[middle] < num && arr[left] < num) {
+            left = middle + 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+    return -1;
 }
+
 
 module.exports = findRotatedIndex
